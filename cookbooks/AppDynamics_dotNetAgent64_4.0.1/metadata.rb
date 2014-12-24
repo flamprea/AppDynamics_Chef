a76e649aed614980e16b6073d93e5a2c268e926b
root <root@chef1.lab1.garden.local>
2014-12-24 00:11:58 -0500

    Version bump

--- cookbooks/AppDynamics_dotNetAgent64_4.0.1/metadata.rb
+++ cookbooks/AppDynamics_dotNetAgent64_4.0.1/metadata.rb
@@ -1,7 +1,7 @@
 name             'AppDynamics_dotNetAgent64_4.0.1'
-maintainer       'YOUR_COMPANY_NAME'
+maintainer       'AppDynamics'
 maintainer_email 'frank.lamprea@appdynamics.com'
 license          'All rights reserved'
 description      'Installs/Configures AppDynamics_dotNetAgent64_4.0.1'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
