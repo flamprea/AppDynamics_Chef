8706c3a53492b455efc792ce9e490843967431dc
Frank Lamprea <frank.lamprea@appdynamics.com>
2014-12-29 21:07:47 -0500

    Enabled Templates

---
+++ cookbooks/AppDynamics_dotNetAgent64_4.0.1/attributes/default.rb
@@ -0,0 +1,8 @@
+default['appdynamics']['tempdir'] = "C:\\Windows\\Temp"
+default['appdynamics']['installdir'] = "C:\\AppDynamics"
+default['appdynamics']['agent']['logdir'] = "C:\\AppDynamics\\DotNetAgent\\Logs"
+default['appdynamics']['controller']['host'] = "172.16.10.81"
+default['appdynamics']['controller']['port'] = "8090"
+default['appdynamics']['application'] = "App123"
+default['appdynamics']['tier'] = "Tier123"
+default['appdynamics']['node'] = "Node123"
